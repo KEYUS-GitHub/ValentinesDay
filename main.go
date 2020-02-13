1aa4363f5425434281e13c8952746eb403c0b8f6
keyus <m13820915363@163.com>
2020-02-13 18:02:58 +0800

    ok

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	addr := "0.0.0.0:8080"
+
+	// 创建gin实例
+	r := gin.Default()
+
+	// 处理静态资源
+	r.Static("/css", "./css")
+	r.Static("/js", "./js")
+	r.StaticFile("/digital.ttf", "./digital.ttf")
+	r.StaticFile("/favicon.ico", "./favicon.ico")
+	r.StaticFile("/", "./index.html")
+
+	// 启动服务
+	log.Fatalln(r.Run(addr))
+}
